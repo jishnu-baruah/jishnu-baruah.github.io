0a8fd266c0f7921af03a83e0b311ab755f1fd22e
Jishnu Baruah <52702792+jishnu-baruah@users.noreply.github.com>
2019-11-17 20:57:01 +0530

    Update garbage.js

--- garbage.js
+++ garbage.js
@@ -7,11 +7,12 @@ function spawnGarbage() {
         swoosh.setVolume(2);
         swoosh.play();
         var garbage = createSprite(100, 100, 10, 10);
-        garbage.x = Math.round(random(50, 350));
+        garbage.x = Math.round(random(width / 2 - 150, width / 2 + 150));
         garbage.velocityY = 7;
         garbage.scale = 0.15;
         garbage.depth = dustbin.depth - 1;
         garbage.lifetime = 88;
+        console.log(garbage);
         var type = setType(no);
         if (no === 5) {
             garbage.shapeColor = (rgb(50, 0, 0));
@@ -102,4 +103,64 @@ function spawnGarbage() {
         // camera.position.y = 700;
     }
     // garbageGroup.bounceOff(dustbin);
+}
+
+function createToxic() {
+    var no = Math.round(random(1, 2));
+    if (frameCount % 180 === 0) {
+        var toxic = createSprite(100, 100);
+        toxic.x = Math.round(random(width / 2 - 150, width + 150));
+        toxic.addImage("toxic", toxicImg);
+        toxic.addImage("battery", battery);
+        if (no === 1) {
+            toxic.changeAnimation("toxic");
+        } else if (no === 2) {
+            toxic.changeAnimation("battery");
+
+
+        }
+        toxic.scale = 0.05;
+        toxic.lifetime = 80;
+        toxic.depth = dustbin.depth - 1;
+        toxic.velocityY = 7;
+        toxic.rotationSpeed = Math.round(random(-15, 15));
+        toxicGroup.push(toxic);
+        gcircle = createSprite(toxic.x, toxic.y);
+        gcircle.velocityY = toxic.velocityY;
+        gcircle.lifetime = 80;
+        gcircle.scale = 0.3;
+        gcircle.addAnimation("1", gcircleImg);
+        gcircle.rotationSpeed = 2;
+        gcircle.depth = toxic.depth - 1;
+        gcircle.setCollider("circle", 0, 0, 80)
+        // gcircle.debug = true;
+        gcircleGroup.push(gcircle);
+
+        tutu.play();
+    }
+    for (var i = 0; i <= toxicGroup.length; i++) {
+
+        var temp = toxicGroup.get(i);
+        var temp2 = gcircleGroup.get(i);
+        if (temp !== undefined) {
+            if (temp.isTouching(dustbin)) {
+                //dustbin.destroy();
+                temp.destroy();
+                tutu.stop();
+                // alert("woho");
+                if (!temp.isTouching(dustbin)) {
+                    o_ou.play();
+                    dustbin.changeAnimation("toxic");
+                    score = score - 1;
+                }
+            }
+        }
+        if (temp2 !== undefined) {
+            if (temp2.isTouching(dustbin)) {
+                //dustbin.destroy();
+                temp2.destroy();
+                // alert("woho");
+            }
+        }
+    }
 }
