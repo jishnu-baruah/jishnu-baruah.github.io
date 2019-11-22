fef52e80f81c2134b092f9e2f168a8a3c5c47c0d
Jishnu Baruah <52702792+jishnu-baruah@users.noreply.github.com>
2019-11-22 10:54:38 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -224,7 +224,7 @@ function draw() {
 
         }
         if (gamestate === "play") {
-            spawnGarbage(15, 20);
+            spawnGarbage(15, 15);
             mouseControl();
             // swipeControl();
             createToxic(180, 9);
